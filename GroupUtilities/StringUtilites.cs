2689bc8fa1b00113e4124d29bfb23b50d461f2a8
AJ <144151461+Ashley-Haltenhoff-Student@users.noreply.github.com>
2024-01-29 19:45:23 +0000

    Methods Set Up

--- GroupUtilities/StringUtilites.cs
+++ GroupUtilities/StringUtilites.cs
@@ -1,8 +1,71 @@
-namespace GroupUtilities {
+namespace GroupUtilities
+{
 
-    public static class StringUtilities {
+    public static class StringUtilities
+    {
 
+
+        public static void ToUpper(string s)
+        {
+
+        }
+
+        public static void ToLower(string s)
+        {
+
+        }
+
+        public static void Length(string s)
+        {
+
+        }
+
+        public static void Reverse(string s)
+        {
+
+        }
+
+        public static int CountVowels(string s)
+        {
+            char[] vowel = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
+            int numberOfVowels = 0;
+
+            foreach (char letter in s)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (letter == vowel[i])
+                    {
+                        numberOfVowels++;
+                    }
+                }
+            }
+
+            return numberOfVowels;
+        }
+
+        public static int CountConsonants(string s)
+        {
+            char[] vowel = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
+            int numberOfConsonants = 0;
+
+            foreach (char letter in s)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (letter != vowel[i])
+                    {
+                        numberOfConsonants++;
+                    }
+                }
+            }
+
+            return numberOfConsonants;
+        }
+
+        public static void HasLetter(string s, char letter)
+        {
+
+        }
     }
-
-    
 }
