7020693cef0b779045ea69ed0969e74b7df223dc
Keagan-Ziemann-Bell-student <144153286+Keagan-Ziemann-Bell-student@users.noreply.github.com>
2024-01-29 18:58:35 +0000

    Started working on the array methods

--- GroupUtilities/ArrayUtilities.cs
+++ GroupUtilities/ArrayUtilities.cs
@@ -2,6 +2,66 @@ namespace GroupUtilities {
     
     public static class ArrayUtilities {
 
-    }
+        public static int[] arrayRes;
+        public static int Average(int[] array) {
+            int total = 0;
+            int amount = 0;
+            foreach (int num in array) {
+                total += num;
+            }
+            foreach (int num in array) {
+                amount++;
+            }
+            int average = total / amount;
+            return average;
+        }
 
-}
+        public static int Max(int[] array) {
+            int Max = array[0];
+            int n = 0;
+            foreach (int num in array) {
+                if (Max < array[n]) {
+                    Max = array[n];
+                }
+                n++;
+            }
+            return Max;
+        }
+
+        public static int Min(int[] array) {
+            int Min = array[0];
+            int n = 0;
+            foreach (int num in array) {
+                if (Min > array[n]) {
+                    Min = array[n];
+                }
+                n++;
+            }
+            return Min;
+        }
+
+        public static int[] SortAscending(int[] array) {
+            int Min = array[0];
+            int amount = 0;
+            int n = 0;
+            foreach (int num in array) {
+                amount++;
+            }
+            for (int i = 0; i < amount -1; i++) {
+                for (n = i+ 1; n< amount; n++) {
+                if (Min > array[n]) {
+                    Min = array[n];
+                }
+            }
+            array[i] = Min;
+            n = 0;
+            }
+            arrayRes = array;
+            return array;
+
+        }
+
+
+        }
+
+    }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@
 using GroupUtilities;
 public class Program {
 public static void Main(string[] args) {
+Console.WriteLine(ArrayUtilities.Average(new int[] {1, 2, 3, 4, 5, 6}));
+Console.WriteLine(ArrayUtilities.Max(new int[] {1, 2, 3, 4, 5, 6}));
+Console.WriteLine(ArrayUtilities.Min(new int[] {1, 2, 3, 4, 5, 6}));
+int[] result = ArrayUtilities.SortAscending(new int[] {1, 2, 3, 4, 5, 6});
+Console.WriteLine(string.Join(", ", ArrayUtilities.arrayRes));
 
 }
 }
