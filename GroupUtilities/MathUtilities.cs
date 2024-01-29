c10e3eb430bfbc3e3f53364eff0ad293452e6ea8
Eli-Robinson-Student <144161475+Eli-Robinson-Student@users.noreply.github.com>
2024-01-29 18:53:39 +0000

    created the starting lines of code for each parameter

--- GroupUtilities/MathUtilities.cs
+++ GroupUtilities/MathUtilities.cs
@@ -1,8 +1,38 @@
 namespace GroupUtilities {
     
-    public static class MathUtilities {
-
+    public static class MathUtilities 
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Min(int x, int y)
+        {
+            return ;
+        }
+        public static int Max(int x, int y)
+        {
+            return ;
+        }
+        public static int AbsoluteValue(int x)
+        {
+            return ;
+        }
+        public static int Power(int Base, int Power)
+        {
+            return ;
+        }
     }
-
-
 }
