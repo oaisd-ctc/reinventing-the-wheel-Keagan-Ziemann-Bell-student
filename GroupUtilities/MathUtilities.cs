ee8d25bcb865dd1141f58e93df5aac0510f93437
Eli-Robinson-Student <144161475+Eli-Robinson-Student@users.noreply.github.com>
2024-01-30 18:41:28 +0000

    Finished creating the functions for the math-focused segment of the project

--- GroupUtilities/MathUtilities.cs
+++ GroupUtilities/MathUtilities.cs
@@ -20,19 +20,97 @@ namespace GroupUtilities {
         }
         public static int Min(int x, int y)
         {
-            return ;
+            int Min = 0;
+            if (x < y)
+            {
+                Min = x;
+            }
+            else
+            {
+                Min = y;
+            }
+            return Min;
         }
         public static int Max(int x, int y)
         {
-            return ;
+            int Max = 0;
+            if (x > y)
+            {
+                Max = x;
+            }
+            else
+            {
+                Max = y;
+            }
+            return Max;
         }
         public static int AbsoluteValue(int x)
         {
-            return ;
+            return -x;
         }
         public static int Power(int Base, int Power)
         {
-            return ;
+            int total = Base;
+            for (int n = 2; n <= Power; n++)
+            {
+                total = total * Base;
+            }
+            return total;
+        }
+         public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public static double Min(double x, double y)
+        {
+            double Min = 0;
+            if (x < y)
+            {
+                Min = x;
+            }
+            else
+            {
+                Min = y;
+            }
+            return Min;
+        }
+        public static double Max(double x, double y)
+        {
+            double Max = 0;
+            if (x > y)
+            {
+                Max = x;
+            }
+            else
+            {
+                Max = y;
+            }
+            return Max;
+        }
+        public static double AbsoluteValue(double x)
+        {
+            return -x;
+        }
+        public static double Power(double Base, double Power)
+        {
+            double total = Base;
+            for (double n = 2; n <= Power; n++)
+            {
+                total = total * Base;
+            }
+            return total;
         }
     }
 }
