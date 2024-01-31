eb44b41e9d2d4bba5f863510764de3381ec4a79e
AJ <144151461+Ashley-Haltenhoff-Student@users.noreply.github.com>
2024-01-31 19:29:14 +0000

    Coded all pre-defined methods in StringUtilities.cs

--- GroupUtilities/StringUtilites.cs
+++ GroupUtilities/StringUtilites.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace GroupUtilities
 {
 
@@ -5,24 +7,77 @@ namespace GroupUtilities
     {
 
 
-        public static void ToUpper(string s)
+        public static string ToUpper(string s)
         {
+            string upperString = "";
 
+            foreach (char c in s)
+            {
+                byte CharInAscii = (byte)c;
+                if (CharInAscii <= 90)
+                {
+                    upperString += c;
+                    continue;
+                }
+                CharInAscii -= 32;
+                char upperChar = (char)CharInAscii;
+
+                upperString += upperChar;
+            }
+
+            return upperString;
         }
 
-        public static void ToLower(string s)
+        public static string ToLower(string s)
         {
+            string lowerString = "";
 
+            foreach (char c in s)
+            {
+                byte CharInAscii = (byte)c;
+                if (CharInAscii > 90)
+                {
+                    lowerString += c;
+                    continue;
+                }
+                CharInAscii += 32;
+                char lowerChar = (char)CharInAscii;
+
+                lowerString += lowerChar;
+            }
+
+            return lowerString;
         }
 
-        public static void Length(string s)
+        public static int Length(string s)
         {
+            int letters = 0;
 
+            foreach (char c in s)
+            {
+                letters++;
+            }
+
+            return letters;
         }
 
-        public static void Reverse(string s)
+        public static string Reverse(string s)
         {
+            char[] array = new char[Length(s)];
+            String reversedString = "";
+            int index = 0;
 
+            foreach (char c in s)
+            {
+                array[index] = c;
+                index++;
+            }
+            for (int i = Length(s) - 1; i >= 0; i--)
+            {
+                reversedString += array[i];
+            }
+
+            return reversedString;
         }
 
         public static int CountVowels(string s)
@@ -47,25 +102,37 @@ namespace GroupUtilities
         public static int CountConsonants(string s)
         {
             char[] vowel = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
-            int numberOfConsonants = 0;
+            int numberOfVowels = 0;
+            bool match = false;
 
             foreach (char letter in s)
             {
                 for (int i = 0; i < 10; i++)
                 {
-                    if (letter != vowel[i])
+                    if (letter == vowel[i])
                     {
-                        numberOfConsonants++;
+                        numberOfVowels++;
                     }
                 }
             }
 
-            return numberOfConsonants;
+            return Length(s) - numberOfVowels;
         }
 
-        public static void HasLetter(string s, char letter)
+        public static bool HasLetter(string s, char letter)
         {
+            bool hasLetter = false;
 
+            foreach (char c in s)
+            {
+                if (c == letter)
+                {
+                    hasLetter = true;
+                    break;
+                }
+            }
+
+            return hasLetter;
         }
     }
 }
