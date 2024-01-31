db1fec460636efae79c5da9f8b3dfeca9536d755
Eli-Robinson-Student <144161475+Eli-Robinson-Student@users.noreply.github.com>
2024-01-31 17:46:17 +0000

    created XML Comments for each of the functions

--- GroupUtilities/MathUtilities.cs
+++ GroupUtilities/MathUtilities.cs
@@ -1,23 +1,53 @@
-namespace GroupUtilities {
-    
-    public static class MathUtilities 
+namespace GroupUtilities
+{
+    public static class MathUtilities
     {
+        /// <summary>
+        /// Adds together two Integers
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Returns the sum of the two numbers</returns>
         public static int Add(int x, int y)
         {
             return x + y;
         }
+        /// <summary>
+        /// Subtracts the two Integers
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Retruns the remainder of the two numbers</returns>
         public static int Subtract(int x, int y)
         {
             return x - y;
         }
+        /// <summary>
+        /// Divides the two Integers
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Returns the quotient of the two numbers</returns>
         public static int Divide(int x, int y)
         {
             return x / y;
         }
+        /// <summary>
+        /// Multiplies the two Integers
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Returns the of product the two numbers</returns>
         public static int Multiply(int x, int y)
         {
             return x * y;
         }
+        /// <summary>
+        /// Gives the minimum of the two Integers
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Returns the Minimum of the two numbers</returns>
         public static int Min(int x, int y)
         {
             int Min = 0;
@@ -31,6 +61,12 @@ namespace GroupUtilities {
             }
             return Min;
         }
+        /// <summary>
+        /// Gives the maximum of the two integers
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Returns the Maximum of the two numbers</returns>
         public static int Max(int x, int y)
         {
             int Max = 0;
@@ -44,10 +80,21 @@ namespace GroupUtilities {
             }
             return Max;
         }
+        /// <summary>
+        /// Gives the abosolute value of the integer
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns>Returns the Absolute Value of the number</returns>
         public static int AbsoluteValue(int x)
         {
             return -x;
         }
+        /// <summary>
+        /// Gives the total of the Base Integer by the power of the Power Integer
+        /// </summary>
+        /// <param name="Base"></param>
+        /// <param name="Power"></param>
+        /// <returns>Returns the Base number to the power of the Power number</returns>
         public static int Power(int Base, int Power)
         {
             int total = Base;
@@ -57,7 +104,7 @@ namespace GroupUtilities {
             }
             return total;
         }
-         public static double Add(double x, double y)
+        public static double Add(double x, double y)
         {
             return x + y;
         }
--- Program.cs
+++ Program.cs
@@ -1,40 +1,41 @@
 ﻿using System;
 using System.Security.AccessControl;
 using GroupUtilities;
-public class Program {
-public static void Main(string[] args) 
+public class Program
 {
-    int result = MathUtilities.Power(2, 4);
-    Console.WriteLine(result);
-    int result2 = MathUtilities.Add(3, 1);
-    Console.WriteLine(result2);
-    int result3 = MathUtilities.Subtract(4, 3);
-    Console.WriteLine(result3);
-    int result4 = MathUtilities.Divide(6, 2);
-    Console.WriteLine(result4);
-    int result5 = MathUtilities.Multiply(3, 2);
-    Console.WriteLine(result5);
-    int result6 = MathUtilities.Min(2, 4);
-    Console.WriteLine(result6);
-    int result7 = MathUtilities.Max(4, 2);
-    Console.WriteLine(result7);
-    int result8 = MathUtilities.AbsoluteValue(5);
-    Console.WriteLine(result8);
-    double result9 = MathUtilities.Power(2, 4);
-    Console.WriteLine(result9);
-    double result10 = MathUtilities.Add(3, 1);
-    Console.WriteLine(result10);
-    double result11 = MathUtilities.Subtract(4, 3);
-    Console.WriteLine(result11);
-    double result12 = MathUtilities.Divide(6, 2);
-    Console.WriteLine(result12);
-    double result13 = MathUtilities.Multiply(3, 2);
-    Console.WriteLine(result13);
-    double result14 = MathUtilities.Min(2, 4);
-    Console.WriteLine(result14);
-    double result15 = MathUtilities.Max(4, 2);
-    Console.WriteLine(result15);
-    double result16 = MathUtilities.AbsoluteValue(5);
-    Console.WriteLine(result16);
-}
+    public static void Main(string[] args)
+    {
+        int result = MathUtilities.Power(2, 4);
+        Console.WriteLine(result);
+        int result2 = MathUtilities.Add(3, 1);
+        Console.WriteLine(result2);
+        int result3 = MathUtilities.Subtract(4, 3);
+        Console.WriteLine(result3);
+        int result4 = MathUtilities.Divide(6, 2);
+        Console.WriteLine(result4);
+        int result5 = MathUtilities.Multiply(3, 2);
+        Console.WriteLine(result5);
+        int result6 = MathUtilities.Min(2, 4);
+        Console.WriteLine(result6);
+        int result7 = MathUtilities.Max(4, 2);
+        Console.WriteLine(result7);
+        int result8 = MathUtilities.AbsoluteValue(5);
+        Console.WriteLine(result8);
+        double result9 = MathUtilities.Power(2, 4);
+        Console.WriteLine(result9);
+        double result10 = MathUtilities.Add(3, 1);
+        Console.WriteLine(result10);
+        double result11 = MathUtilities.Subtract(4, 3);
+        Console.WriteLine(result11);
+        double result12 = MathUtilities.Divide(6, 2);
+        Console.WriteLine(result12);
+        double result13 = MathUtilities.Multiply(3, 2);
+        Console.WriteLine(result13);
+        double result14 = MathUtilities.Min(2, 4);
+        Console.WriteLine(result14);
+        double result15 = MathUtilities.Max(4, 2);
+        Console.WriteLine(result15);
+        double result16 = MathUtilities.AbsoluteValue(5);
+        Console.WriteLine(result16);
+    }
 }
